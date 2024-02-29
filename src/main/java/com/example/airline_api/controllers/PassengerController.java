5ce0e5545864b18838b79b94d1654a6780fd411f
Monesha <moneshadhali@gmail.com>
2024-02-29 17:28:24 +0000

    added passenger services

--- src/main/java/com/example/airline_api/controllers/PassengerController.java
+++ src/main/java/com/example/airline_api/controllers/PassengerController.java
@@ -1,31 +1,39 @@
 package com.example.airline_api.controllers;
 
 import com.example.airline_api.models.Passenger;
+import com.example.airline_api.services.PassengerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/passengers")
 public class PassengerController {
 
+    PassengerService passengerService;
     // Display details of all passengers
     @GetMapping
     public ResponseEntity<List<Passenger>> getAllPassengers(){
-        return null;
+        return new ResponseEntity<>(passengerService.getAllPassengers(), HttpStatus.OK);
     }
 
     // Display specific passenger details
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Passenger> getPassengerById(){
-        return null;
+    public ResponseEntity<Passenger> getPassengerById(@PathVariable Long id){
+        Optional<Passenger> passenger = passengerService.getPassengerById(id);
+        if(passenger.isPresent()){
+            return new ResponseEntity<>(passenger.get(),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null ,HttpStatus.NOT_FOUND);
     }
 
     // Add a new passenger
     @PostMapping
-    public ResponseEntity<Passenger> addNewPassenger(){
-        return null;
+    public ResponseEntity<Passenger> addNewPassenger(@RequestBody Passenger passenger){
+        return new ResponseEntity<>(passengerService.addNewPassenger(passenger) ,HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/airline_api/services/PassengerService.java
+++ src/main/java/com/example/airline_api/services/PassengerService.java
@@ -1,8 +1,12 @@
 package com.example.airline_api.services;
 
+import com.example.airline_api.models.Passenger;
 import com.example.airline_api.repositories.PassengerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PassengerService {
@@ -10,4 +14,15 @@ public class PassengerService {
     PassengerRepository passengerRepository;
 
 
+    public List<Passenger> getAllPassengers() {
+        return passengerRepository.findAll();
+    }
+
+    public Optional<Passenger> getPassengerById(Long id) {
+        return passengerRepository.findById(id);
+    }
+
+    public Passenger addNewPassenger(Passenger passenger) {
+        return passengerRepository.save(passenger);
+    }
 }
