0f290e2f8f4243f65a23c4a18b595ad0d3d0758b
Monesha <moneshadhali@gmail.com>
2024-02-29 16:27:35 +0000

    adding data loader components

---
+++ src/main/java/com/example/airline_api/components/DataLoader.java
@@ -0,0 +1,13 @@
+package com.example.airline_api.components;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
